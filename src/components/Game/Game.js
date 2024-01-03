2592041e30a1edf2f9d81c173b94fff09ea57bba
Colleen Geohagan <colleen.geohagan@voxmedia.com>
2024-01-03 11:18:10 -0500

    finished the game

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -4,6 +4,8 @@ import { sample } from '../../utils';
 import { WORDS } from '../../data';
 import GuessInput from '../GuessInput';
 import GuessResults from '../GuessResults';
+import WonBanner from '../WonBanner';
+import LostBanner from '../LostBanner';
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants';
 
 // Pick a random word on every pageload.
@@ -13,13 +15,17 @@ console.info({ answer });
 
 function Game() {
   const [guesses, setGuesses] = React.useState([]);
+  const [gameStatus, setGameStatus] = React.useState('running');
 
   function handleSubmitGuess(tentativeGuess) {
-    if (guesses.length >= NUM_OF_GUESSES_ALLOWED) {
-      console.log('Hello! Sorry you have reached the max number of guesses');
-      return;
+    const newGuesses = [...guesses, tentativeGuess];
+    setGuesses(newGuesses);
+
+    if (tentativeGuess.toUpperCase() === answer) {
+      setGameStatus('won');
+    } else if (newGuesses.length === NUM_OF_GUESSES_ALLOWED) {
+      setGameStatus('lost');
     }
-    setGuesses([...guesses, tentativeGuess]);
   }
 
   return (
@@ -29,7 +35,12 @@ function Game() {
         answer={answer}
         maxNumGuesses={NUM_OF_GUESSES_ALLOWED}
       />
-      <GuessInput handleSubmitGuess={handleSubmitGuess} />
+      <GuessInput
+        handleSubmitGuess={handleSubmitGuess}
+        gameStatus={gameStatus}
+      />
+      {gameStatus === 'won' && <WonBanner numGuesses={guesses.length} />}
+      {gameStatus === 'lost' && <LostBanner answer={answer} />}
     </>
   );
 }
